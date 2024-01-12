9b93026907d5f6c17ea4e892852ec7e80e01009b
songryel-park <fuf3413@gmail.com>
2024-01-12 16:12:01 +0900

    JWT

---
+++ src/main/kotlin/com/teamsparta/commerce/security/SecurityConfig.kt
@@ -0,0 +1,37 @@
+package com.teamsparta.commerce.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@Configuration
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .httpBasic { it.disable() }
+            .csrf { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests {
+                it.requestMatchers("/login", "/signup", "/h2-console/**").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .addFilterBefore(
+                JwtAuthenticationFilter(jwtTokenProvider),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder =
+        PasswordEncoderFactories.createDelegatingPasswordEncoder()
+}
---
+++ src/main/kotlin/com/teamsparta/commerce/security/TokenInfo.kt
@@ -0,0 +1,3 @@
+package com.teamsparta.commerce.security
+
+data class TokenInfo(val grantType: String, val accessToken: String)

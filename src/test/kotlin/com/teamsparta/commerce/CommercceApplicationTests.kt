eae2469459541635f171c3952ea0d3ffec564c7e
songryel-park <fuf3413@gmail.com>
2024-01-12 16:13:14 +0900

    initial commit

---
+++ src/test/kotlin/com/teamsparta/commerce/CommercceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.teamsparta.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommercceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
